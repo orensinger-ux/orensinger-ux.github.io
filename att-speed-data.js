3aef3daf8f386c9ba95579309bc1cde0f714b36e
orensinger-ux <oren.singer@gmail.com>
2026-09-07 21:49:33 -0700

    Add Sep 7 evening test (final)

--- att-speed-data.js
+++ att-speed-data.js
@@ -5,6 +5,9 @@
 // down/up = Mbps (null = missed test). note = "" or short text.
 const SPEED_TESTS = [
   
+  {"t": "Sep 7, 9:47 PM", "slot": "evening", "down": 1298, "up": 1327, "note": "fired ~45 min late"},
+
+  
   {"t": "Sep 7, 1:56 PM", "slot": "midday", "down": 1275, "up": 1275, "note": "fired ~45 min late"},
 
   
